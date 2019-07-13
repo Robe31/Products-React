763beb6819f5c2dff5f7f32e9ce464108166d9df
Jerobe Robinson <jerobe.rob@gmail.com>
2019-07-13 15:48:17 -0500

    add a product list

---
+++ src/components/productList.js
@@ -0,0 +1,142 @@
+const products = [
+    {
+        id: "1", 
+        name: "Pencil", 
+        price: 2, 
+        desc: "", 
+        imgUrl: ""
+    },    
+    {
+        id: "2", 
+        name: "Paper", 
+        price: 5, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "3", 
+        name: "Tissue", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "4", 
+        name: "Bottle Water", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "5", 
+        name: "Ground Beef", 
+        price: 9, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "6", 
+        name: "Chedder Cheese", 
+        price: 4, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "7", 
+        name: "Hamburger Buns", 
+        price: 1, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "8", 
+        name: "Lettuce", 
+        price: 1, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "9", 
+        name: "Mayo", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "10", 
+        name: "Ketchup", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "11", 
+        name: "Popcorn", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "12", 
+        name: "Fries", 
+        price: 5, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "13", 
+        name: "Canola Oil", 
+        price: 4, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "14", 
+        name: "Hot Dogs", 
+        price: 6, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "15", 
+        name: "Pens", 
+        price: 1, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "16", 
+        name: "Du-rag", 
+        price: 2, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "17", 
+        name: "Paper Towels", 
+        price: 3, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "18", 
+        name: "Gold Chain", 
+        price: 250, 
+        desc: "", 
+        imgUrl: ""
+    },
+    {
+        id: "19", 
+        name: "Sushi", 
+        price: 5, 
+        desc: "", 
+        imgUrl: ""
+    },    
+    {
+        id: "20", 
+        name: "Cookies", 
+        price: 4, 
+        desc: "", 
+        imgUrl: ""
+    },    
+]
