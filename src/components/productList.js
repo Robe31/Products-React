0ad63dfa4e27ea3f44d1ecad4a4b2597f63360df
Jerobe Robinson <jerobe.rob@gmail.com>
2019-07-13 17:41:00 -0500

    Added desc and imgUrl to productList

--- src/components/productList.js
+++ src/components/productList.js
@@ -3,140 +3,140 @@ const products = [
         id: "1", 
         name: "Pencil", 
         price: 2, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Yupp, you can write with this!", 
+        imgUrl: "https://via.placeholder.com/150"
     },    
     {
         id: "2", 
         name: "Paper", 
         price: 5, 
-        desc: "", 
-        imgUrl: ""
+        desc: "This peice of magic lets you record information", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "3", 
         name: "Tissue", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Gotta wiip ya ass", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "4", 
         name: "Bottle Water", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Best thing since fresh water!", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "5", 
         name: "Ground Beef", 
         price: 9, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Poor cow, Rich stomach", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "6", 
         name: "Chedder Cheese", 
         price: 4, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Move back Chester, Chedder is here", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "7", 
         name: "Hamburger Buns", 
         price: 1, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Put some meat between those buns", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "8", 
         name: "Lettuce", 
         price: 1, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Papper water basically", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "9", 
         name: "Mayo", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "I dont know what it is but it's delis :P", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "10", 
         name: "Ketchup", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Almost looks like blood", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "11", 
         name: "Popcorn", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Its popcorn time baby yeaaaa", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "12", 
         name: "Fries", 
         price: 5, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Chopped up roots", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "13", 
         name: "Canola Oil", 
         price: 4, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Compressed veggies", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "14", 
         name: "Hot Dogs", 
         price: 6, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Mix of all the meats into one", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "15", 
         name: "Pens", 
         price: 1, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Pencil 2.0", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "16", 
         name: "Du-rag", 
         price: 2, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Gotta kind them waves on point ya dig", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "17", 
         name: "Paper Towels", 
         price: 3, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Greatest invention known to man", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "18", 
         name: "Gold Chain", 
         price: 250, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Gotta flex wit da chain on my neck", 
+        imgUrl: "https://via.placeholder.com/150"
     },
     {
         id: "19", 
         name: "Sushi", 
         price: 5, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Shushi get in my belly", 
+        imgUrl: "https://via.placeholder.com/150"
     },    
     {
         id: "20", 
         name: "Cookies", 
         price: 4, 
-        desc: "", 
-        imgUrl: ""
+        desc: "Butter cookies only", 
+        imgUrl: "https://via.placeholder.com/150"
     },    
 ]
