b030b0f48bdc14ac85b2c93889ea0ad5d8fafe5e
Jerobe Robinson <jerobe.rob@gmail.com>
2019-07-13 19:04:58 -0500

    v1.01

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,7 +8,8 @@ function Product(){
                 <img src={item.imgUrl} alt={item.name} className="product_img"/>
                 <div className="product_text">
                     <h3 className="product_name">{item.name}</h3>
-                    <p className="product_price">{item.price}</p>
+                    <p className="product_price">${item.price}</p>
+                    <hr/>
                     <p className="product_desc">{item.desc}</p>
                 </div>
             </div>
